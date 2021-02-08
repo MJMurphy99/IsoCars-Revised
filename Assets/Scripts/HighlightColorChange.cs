608a5218c55250c419fe3b81a2eeacb3ecc43fc2
MJMurphy99 <43189660+MJMurphy99@users.noreply.github.com>
2021-02-08 11:34:47 -0500

    Hover Over Effect/Filler Gameplay son
    It's mario kart music nintendo don't sue me pls

---
+++ Assets/Scripts/HighlightColorChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightColorChange : MonoBehaviour
+{
+    public Color baseColor;
+    public Color hoverColor;
+    public bool mouseOver = false;
+
+    public void OnMouseEnter()
+    {
+        mouseOver = true;
+        GetComponent<Renderer>().material.SetColor("_Color", hoverColor);
+    }
+
+    public void OnMouseExit()
+    {
+        mouseOver = false;
+        GetComponent<Renderer>().material.SetColor("_Color", baseColor);
+    }
+}
